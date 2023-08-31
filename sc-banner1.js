9bbc0adfb74222330395498b26c84159e3bffaa0
Wellcandzzz <139671531+Wellcandzzz@users.noreply.github.com>
2023-08-31 23:41:40 +0700

    Update and rename sc-banner1.js to sc-speedtest.js

--- sc-banner1.js
+++
@@ -1,8 +0,0 @@
-  atOptions = {
-		'key' : 'a453f3c1c54a533d5c4976d68b3d3d83',
-		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://splendidsugar.com/a453f3c1c54a533d5c4976d68b3d3d83/invoke.js"></scr' + 'ipt>');
