bf49761e084c0c8b8105ff04d53ae621e3e80474
Wellcandzzz <139671531+Wellcandzzz@users.noreply.github.com>
2023-12-08 23:16:25 +0700

    Create navbar ruangapp.js

---
+++ navbar ruangapp.js
@@ -0,0 +1,11 @@
+<input class="tbIn hidden" id="fTabs-1" type="radio" name="postTabs" checked="">
+<input class="tbIn hidden" id="fTabs-2" type="radio" name="postTabs">
+<input class="tbIn hidden" id="fTabs-3" type="radio" name="postTabs">
+<input class="tbIn hidden" id="fTabs-4" type="radio" name="postTabs">
+<div class="tbHd scrlH" style="justify-content: center;">
+<label for="fTabs-1" data-text="Info"></label>
+<label for="fTabs-2" data-text="Deskripsi"></label>
+<label for="fTabs-3" data-text="Picture"></label>
+<label for="fTabs-4" data-text="Download"></label>
+</div>
+<div class="tbCn">
